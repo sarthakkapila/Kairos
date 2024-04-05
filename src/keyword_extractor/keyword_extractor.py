0908c7bfbba5b7c4f2a4bcb465e2066f667539d9
Sarthak Kapila <sarthakkapila27x@gmail.com>
2024-04-05 19:47:09 +0530

    fixes

--- src/keyword_extractor/keyword_extractor.py
+++ src/keyword_extractor/keyword_extractor.py
@@ -1,20 +1,37 @@
 from keybert import KeyBERT
 
-# Using BERT to extract keywords
 class SentenceBert:
     def __init__(self):
         self.kw_model = KeyBERT()
 
     def extract_keywords(self, sentence, top_n: int = 5) -> list:
-        keywords = self.kw_model.extract_keywords(
-            sentence,
-            keyphrase_ngram_range=(1, 1),
-            stop_words="english",
-            top_n=top_n,
-            use_mmr=True,
-            diversity=0.7,
-        )
+        """
+        Extracts keywords from a given sentence using BERT embeddings.
 
-        keywords = [word[0] for word in keywords]
+        Args:
+            sentence (str): The input sentence from which keywords are to be extracted.
+            top_n (int): The number of top keywords to be extracted. Default is 5.
 
-        return keywords
+        Returns:
+            list: A list of extracted keywords.
+        """
+        if not sentence:
+            return []
+
+        try:
+            keywords = self.kw_model.extract_keywords(
+                sentence,
+                keyphrase_ngram_range=(1, 1),
+                stop_words="english",
+                top_n=top_n,
+                use_mmr=True,
+                diversity=0.7,
+            )
+
+            keywords = [word[0] for word in keywords]
+
+            return keywords
+
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return []
---
+++ src/llm/llm.py
@@ -0,0 +1,28 @@
+import os
+from langchain_community.chat_models import ChatCohere
+from langchain_core.output_parsers import StrOutputParser
+import google.generativeai as genai
+
+
+class LLM:
+    def __init__(self, base_model, api_key) -> None:
+        self.base_model = base_model
+
+        if base_model == "Gemini-Pro":
+            os.environ["GOOGLE_API_KEY"] = api_key
+            genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
+            self.model = genai.GenerativeModel("gemini-pro")
+        else:
+            os.environ["COHERE_API_KEY"] = api_key
+            self.model = ChatCohere()
+
+    def inference(self, prompt):
+        if self.base_model == "Gemini-Pro":
+            response = self.model.generate_content(prompt).text
+        elif self.base_model == "Cohere":
+            chain = self.model | StrOutputParser()
+            response = chain.invoke(prompt)
+
+        return response
+
+
